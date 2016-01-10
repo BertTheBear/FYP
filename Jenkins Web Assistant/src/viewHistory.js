cf18cfab02faffa4ebc9991c6e650078308c28a8
BertTheBear <stormfly@live.ie>
2016-01-10 20:44:58 +0000

    Minor alterations
    Changed version name and removed list from testing in viewHistory files.

--- Jenkins Web Assistant/src/viewHistory.js
+++ Jenkins Web Assistant/src/viewHistory.js
@@ -1,3 +1,9 @@
+//id of extension
+var myid = chrome.runtime.id;
+
+
+
+
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -33,90 +39,10 @@ function buildPopupDom(divName, data) {
 	}
 }
 
-// Search history to find up to ten links that a user has typed in,
-// and show those links in a popup.
-function buildHistoryList(divName) {
-	// To look for history items visited in the last week,
-	// subtract a week of microseconds from the current time.
-	var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-	var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
-
-	// Track the number of callbacks from chrome.history.getVisits()
-	// that we expect to get.	When it reaches zero, we have all results.
-	var numRequestsOutstanding = 0;
-
-	chrome.history.search({
-			'text': '',							// Return every history item....
-			'startTime': oneWeekAgo	// that was accessed less than one week ago.
-		},
-		function(historyItems) {
-			// For each history item, get details on all visits.
-			for (var i = 0; i < historyItems.length; ++i) {
-				var url = historyItems[i].url;
-				var processVisitsWithUrl = function(url) {
-					// We need the url of the visited item to process the visit.
-					// Use a closure to bind the	url into the callback's args.
-					return function(visitItems) {
-						processVisits(url, visitItems);
-					};
-				};
-				chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
-				numRequestsOutstanding++;
-			}
-			if (!numRequestsOutstanding) {
-				onAllVisitsProcessed();
-			}
-		});
-
-
-	// Maps URLs to a count of the number of times the user typed that URL into
-	// the omnibox.
-	var urlToCount = {};
-
-	// Callback for chrome.history.getVisits().	
-	// shows all urls visited
-	var processVisits = function(url, visitItems) {
-		for (var i = 0, ie = visitItems.length; i < ie; ++i) {
-			//ignore reloaded pages
-			if (visitItems[i].transition === 'reload') {
-				continue;
-			}
-
-			if (!urlToCount[url]) {
-				urlToCount[url] = 0;
-			}
-
-			urlToCount[url]++;
-		}
-
-		// If this is the final outstanding call to processVisits(),
-		// then we have the final results.	Use them to build the list
-		// of URLs to show in the popup.
-		if (!--numRequestsOutstanding) {
-			onAllVisitsProcessed();
-		}
-	};
-
-	// This function is called when we have the final list of URls to display.
-	var onAllVisitsProcessed = function() {
-		// Get the top scorring urls.
-		urlArray = [];
-		for (var url in urlToCount) {
-			urlArray.push(url);
-		}
-
-		// Sort the URLs by the number of times the user typed them.
-		urlArray.sort(function(a, b) {
-			return urlToCount[b] - urlToCount[a];
-		});
-
-		buildPopupDom(divName, urlArray.slice(0, 50));
-	};
-}
 
 document.addEventListener('DOMContentLoaded', function () {
-	buildHistoryList("historyList");
 	processHistory("commonlyFound");
+	buildIgnoreList("blacklist");
 });
 
 
@@ -207,24 +133,18 @@ function processHistory(divName) {
 		//forProcessing.pathname; // => "/pathname/"
 		//forProcessing.hash;     // => "#hash"
 		//forProcessing.search;   // => "?search=test"
-	}
+	};
 	var printList = function() {
 		//define array to be printed
-		wordArray = []
+		wordArray = [];
 
 		for (var url in urlToCount) {
-			wordArray.push(url);
+			var word = getHostname(url);
+			if (word == myid)
+				word = "Jenkins Web Assistant";
+			wordArray.push(word);
 		}
-
-		//Sort the URLs by the number of times the user typed them.
-		wordArray.sort(function(a, b) {
-			return urlToCount[b] - urlToCount[a];
-		});
-
-		//replace URLs with just the hostname
-		for (var i = 0; i < wordArray.length; i++) {
-			wordArray[i] = getHostname(wordArray[i]);
-		}
+		//END TESTING ============
 
 		//Remove duplicates
 		wordArray = uniq(wordArray);
@@ -232,8 +152,8 @@ function processHistory(divName) {
 
 
 		//Slices to first 10 words encountered and sends to be printed into list
-		buildPopupDom(divName, wordArray.slice(0,50));
-	}
+		buildPopupDom(divName, wordArray.slice(0,25));
+	};
 };
 
 
@@ -244,4 +164,24 @@ function uniq(array) {
     return array.filter(function(item) {
         return seen.hasOwnProperty(item) ? false : (seen[item] = true);
     });
+}
+
+////Retrieves and displays blacklist
+function buildIgnoreList(divName) {
+		chrome.storage.sync.get({
+		ignored: ""
+	}, function(items) {
+		var blacklist = document.getElementById(divName);
+		var data = items.ignored.split(",");
+
+		var ul = document.createElement('ul');
+		blacklist.appendChild(ul);
+
+		for (var i = 0, ie = data.length; i < ie; ++i) {
+			var li = document.createElement('li');
+			li.appendChild(document.createTextNode(data[i]));
+
+			ul.appendChild(li);
+		}
+	});
 }
