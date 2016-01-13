bee13f51c358da758f1078d1708455c6de0ef2dd
BertTheBear <stormfly@live.ie>
2016-01-13 15:52:51 +0000

    Fixed Options link
    Options link now redirects to Extensions page rather than bare HTML

--- Jenkins Web Assistant/src/popup.js
+++ Jenkins Web Assistant/src/popup.js
@@ -1,2 +1,10 @@
 //id of extension
 var myid = chrome.runtime.id;
+
+
+
+document.getElementById('opts').addEventListener('click', open_options);
+
+function open_options() {
+	chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + myid });
+}
